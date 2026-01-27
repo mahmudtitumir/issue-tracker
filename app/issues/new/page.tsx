a8e4b4b8d5b334e79797c02443fa89053845fb18
mahmudtitumir <titumir947@gmail.com>
2026-01-28 05:55:33 +0600

    feat: implement new issue form with title and description, integrate axios for API submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,37 @@
 'use client';
 import { Button, TextField } from '@radix-ui/themes';
-import SimpleMDE from 'react-simplemde-editor';
+import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
+import { useRouter } from 'next/navigation';
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, control } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-4">
-      <TextField.Root placeholder="Title" />
-      <SimpleMDE placeholder="Description..." />
+    <form
+      className="max-w-xl space-y-4"
+      onSubmit={handleSubmit(async data => {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register('title')} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description..." {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
