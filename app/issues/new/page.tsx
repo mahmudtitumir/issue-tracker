53af3c112fe920eafbcd7d500ef30808dab7ae3a
mahmudtitumir <titumir947@gmail.com>
2026-02-05 11:28:04 +0600

    feat: refactor issue creation validation and integrate zod with react-hook-form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,28 @@
 'use client';
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { issueCreateSchema } from '@/app/validationSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import SimpleMDE from 'react-simplemde-editor';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof issueCreateSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
   const [error, setError] = useState('');
-  const { register, handleSubmit, control } = useForm<IssueForm>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(issueCreateSchema),
+  });
   return (
     <div className="max-w-xl">
       {error && (
@@ -29,12 +36,18 @@ const NewIssuePage = () => {
           try {
             await axios.post('/api/issues', data);
             router.push('/issues');
-          } catch (_error) {
+          } catch (error) {
+            console.log(error);
             setError('An Unexpected Error Occurred!');
           }
         })}
       >
         <TextField.Root placeholder="Title" {...register('title')} />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -42,6 +55,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description..." {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
