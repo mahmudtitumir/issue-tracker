3a47045c6b197b989dbbbc11cc550357b6078df2
mahmudtitumir <titumir947@gmail.com>
2026-03-24 14:38:47 +0600

    feat: refactor issue retrieval to use cached function for improved performance

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,19 +7,21 @@ import DeleteIssueButton from './DeleteIssueButton';
 import { getServerSession } from 'next-auth';
 import authOptions from '@/app/auth/authOptions';
 import AssigneeSelect from './AssigneeSelect';
+import { cache } from 'react';
+import { type Metadata } from 'next';
 
 interface Props {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
+
+const getIssue = cache((issueId: string) =>
+  prisma.issue.findUnique({ where: { id: parseInt(issueId) } }),
+);
 
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
   const { id } = await params;
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
+  const issue = await getIssue(id);
   if (!issue) notFound();
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="5">
@@ -39,13 +41,13 @@ const IssueDetailPage = async ({ params }: Props) => {
   );
 };
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
   const { id } = await params;
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
+  const issue = await getIssue(id);
   if (!issue) return {};
   return {
     title: `Issue - ${issue.id}`,
